450bb6e44646c85d752cc290026549c175656c17
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-11-03 22:59:38 +0000

    create test files in js folder

---
+++ js/test.js
@@ -0,0 +1,17 @@
+// ================= TEST FOR ADDING A NEW TASK ================= 
+
+
+// Add tasks to a list so that I can keep track of them
+test("Submitting a new task adds it to the list", () => {
+    // test goes here
+  });
+
+// Test that an empty task is not added
+
+// Test that the input is cleared after adding a task
+
+// Test that the task input is focused after adding a task
+
+// Test for the correct number of tasks after multiple additions
+
+//Test to ensure tasks are added in the correct order
