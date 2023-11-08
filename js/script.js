1548a28eee5ad7b2ff0e448a72c949bf59b37a77
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-11-08 23:25:43 +0000

    style the app and add footer

--- js/script.js
+++ js/script.js
@@ -11,12 +11,15 @@ const todoList = [];
 
 // ================= EVENT LISTENERS =================
 // Event listener for the 'Add' button click event
-    addButton.addEventListener('click', () => {
-        if(taskInput.value.length > 0){
-            addTask()
-        }
-        upDateDom()
-    }); 
+addButton.addEventListener('click', () => {
+    const taskValue = taskInput.value.trim();
+    if(taskValue){
+        addTask(taskValue);
+        upDateDom();
+        taskInput.value = "";
+    }
+}); 
+
 
 // ================= TASK MANIPULATION =================
 // Function to add a new task to the todoList array
@@ -25,20 +28,25 @@ function addTask() {
     newTask.title = taskInput.value;
     newTask.state = "not-started";
     todoList.push(newTask);
-    console.log(todoList);
-
     taskInput.value = "";
 }
 
 // ================= DOM UPDATES =================
 function upDateDom() {
     notStartedContainer.innerHTML = '';  // Clear it before appending new tasks
+
     // Filter the todoList to find tasks with the 'not-started' state
-    todoList.filter(item => {
-        if(item.state = "not-started"){
-            const todoTile = document.createElement("div")
-            todoTile.innerHTML = item.title
-            notStartedContainer.appendChild(todoTile); // Append the new task div to the notStartedContainer
-        }
-    })
+    const notStartedTasks = todoList.filter(item => item.state === "not-started");
+    
+
+    // Iterate over the filtered tasks and append them to the DOM
+notStartedTasks.forEach(item => {
+    const todoTile = document.createElement("div");
+    todoTile.className = 'task'; // Class for the count
+    todoTile.innerHTML = item.title; 
+    notStartedContainer.appendChild(todoTile); 
+});
+
+    // Update the count for not-started tasks
+    document.getElementById('not-started-count').textContent = notStartedTasks.length;
 }
