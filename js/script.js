0ad01ed506925a5473d351a97db6cb31d9984238
Daniel Aliu <danielaliu202@gmail.com>
2023-11-13 00:19:21 +0000

    Updating tasks and deleting them

--- js/script.js
+++ js/script.js
@@ -7,16 +7,19 @@ const completeContainer = document.getElementById('complete-container')
 
 
 // ================= DATA STORAGE =================
-const todoList = [];
+let todoList = [];
 
 // ================= EVENT LISTENERS =================
 // Event listener for the 'Add' button click event
-    addButton.addEventListener('click', () => {
-        if(taskInput.value.length > 0){
-            addTask()
-        }
-        upDateDom()
-    }); 
+addButton.addEventListener('click', () => {
+    if(taskInput.value.length > 0 ){
+        addTask()
+    }
+    notStartedContainer.innerHTML = '';
+    inProgressContainer.innerHTML = '';
+    completeContainer.innerHTML = '';
+    upDateDom()
+}); 
 
 
 // ================= TASK MANIPULATION =================
@@ -31,14 +34,125 @@ function addTask() {
 
 // ================= DOM UPDATES =================
 function upDateDom() {
-    notStartedContainer.innerHTML = '';  // Clear it before appending new tasks
 
     // Filter the todoList to find tasks with the 'not-started' state
-    todoList.filter(item => {
-        if(item.state = "not-started"){
-            const todoTile = document.createElement("div")
-            todoTile.innerHTML = item.title
-            notStartedContainer.appendChild(todoTile); // Append the new task div to the notStartedContainer
-        }
+      
+    todoList.filter(item => item.state == "not-started").forEach(item => {
+        const template = document.getElementById('todo-template');
+        const todoTemplate = template.content.cloneNode(true);
+        const templateTitle = todoTemplate.querySelector(".task-text #text-field");
+        const option1 = todoTemplate.querySelector('.task-tile #start');
+        const option2 = todoTemplate.querySelector('.task-tile #in-prog');
+        const option3 = todoTemplate.querySelector('.task-tile #done');
+        const option4 = todoTemplate.querySelector('.task-tile #delete');
+        templateTitle.textContent = item.title;
+        notStartedContainer.appendChild(todoTemplate)
+
+        testing1(option1, option2, option3, option4)
     })
+
+
+    // Filter the todoList to find tasks with the 'in-progress' state
+
+
+    todoList.filter(item => item.state == "in-progress").forEach(item => {
+        const template = document.getElementById('todo-template');
+        const todoTemplate = template.content.cloneNode(true);
+        const templateTitle = todoTemplate.querySelector(".task-text #text-field");
+        const option1 = todoTemplate.querySelector('.task-tile #start');
+        const option2 = todoTemplate.querySelector('.task-tile #in-prog');
+        const option3 = todoTemplate.querySelector('.task-tile #done');
+        const option4 = todoTemplate.querySelector('.task-tile #delete');
+        templateTitle.textContent = item.title;
+        inProgressContainer.appendChild(todoTemplate)
+        
+        testing1(option1, option2, option3, option4)
+        
+    })
+    
+    // Filter the todoList to find tasks with the 'completed' state
+
+    todoList.filter(item => item.state == "completed").forEach(item => {
+        const template = document.getElementById('todo-template');
+        const todoTemplate = template.content.cloneNode(true);
+        const templateTitle = todoTemplate.querySelector(".task-text #text-field");
+        const option1 = todoTemplate.querySelector('.task-tile #start');
+        const option2 = todoTemplate.querySelector('.task-tile #in-prog');
+        const option3 = todoTemplate.querySelector('.task-tile #done');
+        const option4 = todoTemplate.querySelector('.task-tile #delete');
+        templateTitle.textContent = item.title;
+        completeContainer.appendChild(todoTemplate)
+        
+        testing1(option1, option2, option3, option4)
+        
+    })
+    
+}
+
+
+function testing1(tar1, tar2, tar3, tar4) {
+
+    // Target for reseting a task
+
+    tar1.addEventListener("click", (e) => {
+        const textSpan = e.target.closest('.task-tile').querySelector('#text-field');
+        
+        console.log(textSpan.textContent);
+    
+        const clickIndex = todoList.findIndex(item => item.title === textSpan.textContent);
+        todoList[clickIndex]["state"] = "not-started"
+
+        notStartedContainer.innerHTML = '';
+        inProgressContainer.innerHTML = '';
+        completeContainer.innerHTML = '';
+        upDateDom()
+    });
+
+    // Target for 'in-progressing' a task
+
+    tar2.addEventListener("click", (e) => {
+        const textSpan = e.target.closest('.task-tile').querySelector('#text-field');
+        
+        console.log(textSpan.textContent);
+    
+        const clickIndex = todoList.findIndex(item => item.title === textSpan.textContent);
+        todoList[clickIndex]["state"] = "in-progress"
+
+        notStartedContainer.innerHTML = '';
+        inProgressContainer.innerHTML = '';
+        completeContainer.innerHTML = '';
+        upDateDom()
+    });
+
+    // Target for 'completing' a task
+
+    tar3.addEventListener("click", (e) => {
+        const textSpan = e.target.closest('.task-tile').querySelector('#text-field');
+        
+        console.log(textSpan.textContent);
+    
+        const clickIndex = todoList.findIndex(item => item.title === textSpan.textContent);
+        todoList[clickIndex]["state"] = "completed"
+
+        notStartedContainer.innerHTML = '';
+        inProgressContainer.innerHTML = '';
+        completeContainer.innerHTML = '';
+        upDateDom()
+    });
+
+    // Target for deleting a task
+
+    tar4.addEventListener("click", (e) => {
+        const textSpan = e.target.closest('.task-tile').querySelector('#text-field');
+        
+        console.log(textSpan.textContent);
+    
+        const clickIndex = todoList.findIndex(item => item.title === textSpan.textContent);
+        todoList.splice(clickIndex,1)
+
+        notStartedContainer.innerHTML = '';
+        inProgressContainer.innerHTML = '';
+        completeContainer.innerHTML = '';
+        upDateDom()
+    });
 }
--- js/test.js
+++ js/test.js
@@ -1,13 +1,13 @@
 // ================= TESTS ================= 
 
 // Test to check if the new task is added to the todoList array
-test('should add a new task object to the todoList array', () => {
-    taskInput.value = 'Test task'; // Simulate user input
-    addTask(); 
-    const newTask1 = todoList[0]["title"]
-    equal(newTask1, 'Test task', 'title should be equal');
-    equal(todoList.length, 1, "length should be equal to 1");
-});
+// test('should add a new task object to the todoList array', () => {
+//     taskInput.value = 'Test task'; // Simulate user input
+//     addTask(); 
+//     const newTask1 = todoList[0]["title"]
+//     equal(newTask1, 'Test task', 'title should be equal');
+//     equal(todoList.length, 1, "length should be equal to 1");
+// });
 
 
   
